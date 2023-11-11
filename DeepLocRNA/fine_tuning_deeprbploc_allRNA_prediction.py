ddcad380524474ee4a35059289a274046ff740a9
Jun Wang <35064020+TerminatorJ@users.noreply.github.com>
2023-11-11 23:00:04 +0100

    Update fine_tuning_deeprbploc_allRNA_prediction.py

--- DeepLocRNA/fine_tuning_deeprbploc_allRNA_prediction.py
+++ DeepLocRNA/fine_tuning_deeprbploc_allRNA_prediction.py
@@ -192,7 +192,7 @@ def predict(fasta, rna_types, batch_size = 2, plot = "False", att_config = None,
     result_df = pd.DataFrame(data = all_y_pred, columns = refs, index = ids)
     #embedding the prediction string
     result_df["Prediction"] = results_str
-    result_df.to_csv(os.path.join(current_path, "output.txt"), sep=" ")
+    result_df.to_csv("output.txt", sep=" ")
     with open('output.md', mode='w') as output:
       output.write('![Predicted results](output.txt)')
 
